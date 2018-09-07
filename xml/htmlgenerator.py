971e1abb56df719140141648ffc3771b02d45159
Dan Mercurio <dmercurio92@gmail.com>
2018-09-07 16:14:21 -0700

    added main function

--- xml/htmlgenerator.py
+++ xml/htmlgenerator.py
@@ -10,15 +10,24 @@ the hotel rates across month intervals.
 '''
 
 from os import walk, path
+from sys import argv as args
 
 class HotelHTMLGenerator(object):
 
     def __init__(self, search_directory, output_directory):
-        self.search_directory = search_directory
-        self.output_directory = output_directory
+        self.search_directory = search_directory or "/home/dan/Documents/lantera/xml/search"
+        self.output_directory = output_directory or "/home/dan/Documents/lantera/xml"
+
+    def dirs(self):
+        return (self.search_directory, self.output_directory)
 
     def scan(self):
         if not path.exists(self.search_directory):
             raise SystemExit('Directory doesn\'t exist.')
 
-
+if (__name__ is "__main__"):
+    if len(args > 1):
+        h = HotelHTMLGenerator(args[1], args[2])
+    else:
+        h = HotelHTMLGenerator()
+    print(h.dirs())
