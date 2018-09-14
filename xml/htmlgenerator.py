560fe5816bcc7b1dd843eba76591994391ebad5f
Dan Mercurio <dmercurio92@gmail.com>
2018-09-14 14:07:23 -0700

    added convenience function on list literal

--- xml/htmlgenerator.py
+++ xml/htmlgenerator.py
@@ -9,10 +9,22 @@ and for each directory containing this file, produce two HTML files
 the hotel rates across month intervals.
 '''
 
+# class extendedList(list):
+#     ''' A list that differs from the original in only one way,
+#         it implements an .each() method that takes a callable as
+#         an argument and modifies the list in place.'''
+#     def each(self, callback):
+#         for index, item in enumerate(self):
+#             self[index] = callback(item)
+# list = extendedList
+def each(iterable, callable):
+    for index, item in enumerate(iterable):
+        callable(iterable, index, item)
+        # ex. lambda x,y,z: x[y] -> z
+
 from os import walk, linesep, getcwd
 import os.path
 from sys import argv as args, stdout
-from string import Template
 try:
     import yattag
 except ImportError:
@@ -58,52 +70,43 @@ class HotelHTMLGenerator(object):
             self.year = year        
 
         if ("--relative" not in args):
-            absolute_dirs = [self._convert_directory_to_absolute(val) for val in self.getDirs().values()]
-            self.setDirs({'output_directory': absolute_dirs[0], 'search_directory': absolute_dirs[1]})
+            absolute_dirs = [os.path.realpath(val) for val in self.getDirs().values()]
+            self.setDirs(dict(zip(self.getDirs().keys(), absolute_dirs)))
+            print(self.getDirs())
+            #self.setDirs({'output_directory': absolute_dirs[0], 'search_directory': absolute_dirs[1]})
 
         # An attribute to hold the paths to discovered search results
-        self.paths = [self._convert_directory_to_absolute(x) for x in self.scan()]
+        self.paths = list()
 
-        self.parse(self.paths)
+        # An attribute to hold the XML parser objects
+        self.parser_objects = list()
 
-
-    def _print(self, text, appendNewline = True):
-        try:
-            assert type(text) is str
-            if (appendNewline):
-                stdout.write(text)
-                stdout.write(linesep)
-            else:
-                stdout.write(text)
-        except AssertionError:
-            raise SystemExit("Called _print with non-string as argument")
+        # An attribute to hold prettified raw XML strings for inspection/introspection
+        self.xml_strings = list()
 
     def help(self):
         helpText = """Usage: python[3] {0} [search directory] [output directory] [--arguments (optional)]{1}
 Pass --relative to disable conversion of relative paths to absolute paths. Pass --year (4 digit year) to use a year other than 2018. Pass -h or --help to print this message""".format(args[0], linesep)
         print(helpText)
-        raise SystemExit()
-
-    def _convert_directory_to_absolute(self, directory):
-        ''' Used in several contexts to convert lists of directories which may
-        have relative paths to their absolute counterparts'''
-        return os.path.realpath(directory)
-
-
+        raise SystemExit
 
     def getDirs(self):
-        ''' Get a hash of the directories we are using. '''
+        ''' Get a hash of the directories we are using for search and output. '''
         return self.dirs
 
     def setDirs(self, dirs):
         try:
             assert isinstance(dirs, dict)
-            self.dirs['search_directory'] = dirs['search_directory']
-            self.dirs['output_directory'] = dirs['output_directory']
-            return self.dirs
+            try:
+                assert (len(dirs.keys()) == 2)
+                self.dirs['search_directory'] = dirs['search_directory']
+                self.dirs['output_directory'] = dirs['output_directory']
+                return self.dirs
+            except AssertionError:
+                raise SystemExit("Attempted to set directories with a dictionary missing keys")
         except AssertionError:
-            print("Attempted to set directories to a non-dictionary object")
-            raise SystemExit
+            raise SystemExit("Attempted to set directories to a non-dictionary object")
+            
         except KeyError:
             raise SystemExit("Attempted to set directories with a dictionary of invalid keys")
 
@@ -112,62 +115,98 @@ Pass --relative to disable conversion of relative paths to absolute paths. Pass 
         return [arg for arg in enumerate(args)]
 
     def scan(self):
-        ''' Generator which scans recursively for a rates.input.xml file and yields their paths '''
+        ''' Scan for input xml files and populate the paths attribute with results '''
 
-        if not os.path.exists(self.getDirs()['search_directory']):
+        # Get the directory to start from
+        search_directory = self.getDirs().get('search_directory')
+
+        # Make sure it actually exists
+        if not os.path.exists(search_directory):
             raise SystemExit("Specified search directory does not exist.")
+        else:
+            def search(search_directory):
+                ''' Generator which scans recursively for a rates.input.xml file and yields their paths '''
+                for root, dirs, files in walk(search_directory):
 
-        for root, dirs, files in walk(self.getDirs()['search_directory']):
+                    # Make the search case insensitive by converting everything to lower case
+                    files = [file.lower() for file in files]
 
-            # Make the search case insensitive by converting everything to lower case
-            files = [file.lower() for file in files]
+                    # Look for our XML file in among the files in the current directory
+                    if self.SEARCH_FILENAME in files:
 
-            # Look for our XML file in among the files in the current directory
-            if self.SEARCH_FILENAME in files:
-                # Assemble a full, absolute path
-                fullpath = os.path.join(root, self.SEARCH_FILENAME)
+                        # Join path and filename
+                        fullpath = os.path.join(root, self.SEARCH_FILENAME)
 
-                # Detect symlinks
-                try:
-                    assert not os.path.islink(fullpath)
-                except AssertionError as e:
-                    print("Search result is a symlink (shortcut). Will use real path instead.")
-                    fullpath = os.path.realpath(fullpath)
-                    continue
-                # Verify we are actually pointing toward a file
-                try:
-                    assert os.path.isfile(fullpath)
-                except AssertionError as e:
-                    print("OS reports search result at {0} is not an actual file. Trying to continue...".format(fullpath))
-                    continue
+                        # Detect symlinks
+                        try:
+                            assert not os.path.islink(fullpath)
+                        except AssertionError as e:
+                            print("Search result is a symlink (shortcut). Will use real path instead.")
+                            fullpath = os.path.realpath(fullpath)
+                            continue
 
-                print("Found {0} at {1}".format(self.SEARCH_FILENAME, fullpath))
+                        # Verify we are actually pointing toward a file
+                        try:
+                            assert os.path.isfile(fullpath)
+                        except AssertionError as e:
+                            print("OS reports search result at {0} is not an actual file. Trying to continue...".format(fullpath))
+                            continue
 
-                yield fullpath
+                        # Try to convert a relative path to an absolute path
+                        fullpath = os.path.realpath(fullpath)
 
-    def parse(self, paths):
+                        # Report result
+                        print("Found {0} at {1}".format(self.SEARCH_FILENAME, fullpath))
+
+                        yield fullpath
+        
+
+        detected_paths = [detected_path for detected_path in search(search_directory)]
+        detected_paths.each(lambda x:print(x))
+        detected_paths.each(lambda x:self.paths.append(x))
+
+        return self
+
+    def parse(self):
         ''' Parse XML files at each found path. Generate HTML from each parser object, and output it'''
-        for xmlfile in paths: 
+        for xmlfile in self.paths: 
             if not os.path.exists(xmlfile) or not os.path.isfile(xmlfile):
                 raise SystemExit("Generate HTML function was called with an invalid path or file.")
             try:
-                with open(xmlfile, 'r') as file:
+                with open(xmlfile, 'r') as file: # Entering file context
+                    
+                    # Read from file
                     contents = file.read()
-                    soup = bs4.BeautifulSoup(contents, "lxml")
-                    hotels = soup.find_all('hotel')
+
+                    # Parse raw XML
+                    hotels = bs4.BeautifulSoup(contents, "lxml").find_all('hotel')
+
                     try: 
-                        assert len(hotels)
+                        assert len(hotels) is not 0
+                        
+
+                        self.xml_strings = [hotel.prettify() for hotel in hotels]
+
+                        self.parser_objects = [hotel for hotel in hotels]
+
                     except AssertionError:
                         raise SystemExit('No <hotel> tags found')
-                    for hotel in hotels:
-                        print(hotel.prettify())
             except IOError:
                 raise SystemExit("Unable to read found XML file.")
+            # Leaving file context, file handler closed    
+        return self
+
+    def generate_html():
+        raise NotImplementedError
 
 if (__name__ == "__main__"):
+    # Create an instance of our worker class
     if len(args) == 3:
-        h = HotelHTMLGenerator(args[1], args[2])
+        hg = HotelHTMLGenerator(args[1], args[2])
     else:
-        h = HotelHTMLGenerator()
-    #print("\narguments: ", str(h.getArgs()), end="\n\n")
-    #print("\npaths: ", [x for x in h.scan()], end="\n\n")
+        hg = HotelHTMLGenerator()
+    #print("\narguments: ", str(hg.getArgs()), end="\n\n")
+    #print("\npaths: ", [x for x in hg.scan()], end="\n\n")
+    
+    # Chain of actions this script is designed to perform
+    hg.scan().parse().generate_html()
